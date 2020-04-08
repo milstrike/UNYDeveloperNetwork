2b172c2fdf96ed2b03618e2b9457c0c1ee44023a
Muhammad Irfan Luthfi <iir_irfan02@hotmail.com>
2020-04-08 23:18:01 +0700

    Segitiga Angka (Eclipse Project)

---
+++ SegitigaAngka/src/SegitigaAngka/SegitigaAngka2.java
@@ -0,0 +1,67 @@
+package SegitigaAngka;
+
+/*
+ * Author: Muhammad Irfan Luthfi
+ * github.com/milstrike
+ */
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class SegitigaAngka2 {
+	
+	private static Scanner getScanner() {
+		return new Scanner(System.in);
+	}
+
+	private static void printSpace(int spaceLength) {
+		for(int i = 0; i < spaceLength; i++) {
+			System.out.print(" ");
+		}
+	}
+	
+	private static void printSegitiga1(int numbers) {
+		for(int i = 0; i < numbers; i++) {
+			System.out.print(i+1);
+		}
+	}
+	
+	private static void printSegitiga2(int numbers) {
+		int charlie = numbers;
+		for(int i=0; i < numbers; i++) {
+			System.out.print(charlie);
+			charlie--;
+		}
+	}
+	
+	private static void createSegitiga(int lengthSegitiga) {
+		int alpha = 1, beta = lengthSegitiga;
+		for(int i = 0; i < lengthSegitiga; i++) {
+			printSegitiga1(alpha);
+			printSpace(beta);
+			System.out.print("|");
+			printSpace(beta);
+			printSegitiga2(alpha);
+			System.out.println("");
+			alpha++;
+			beta--;
+		}
+	}
+	
+	public static void main(String[] args) {
+		boolean status = true;
+		System.out.println("PROGRAM SEGITIGA ANGKA \nCoded in java by Muhammad Irfan Luthfi\ngithub.com/milstrike");
+		System.out.println("");
+		while(status) {
+			try {
+				System.out.print("Masukkan tinggi segitiga: ");
+				createSegitiga(getScanner().nextInt());
+				status = false;
+			}catch(InputMismatchException e) {
+				System.out.println("Masukkan harus berupa angka!");
+				status = true;
+			}
+		}
+	}
+
+}
